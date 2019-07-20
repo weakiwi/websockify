0752c196a2e4d06f3ed8faccdad2c4220903b36f
Deng Yi <yi.deng@yideng-mac.local>
2019-07-20 16:09:31 +0800

    add totp support

--- websockify/auth_plugins.py
+++ websockify/auth_plugins.py
@@ -1,3 +1,6 @@
+import pyotp
+import base64
+
 class BasePlugin(object):
     def __init__(self, src=None):
         self.source = src
@@ -28,6 +31,56 @@ class InvalidOriginError(AuthenticationError):
             log_msg="Invalid Origin Header: Expected one of "
                     "%s, got '%s'" % (expected, actual))
 
+SECRET='this_is_my_pc'
+
+class OTPAuth(object):
+    """Verifies Basic Auth headers. Specify src as username:password"""
+
+    def __init__(self, src=None):
+        secret = base64.b32encode(SECRET)
+        totp = pyotp.TOTP(secret)
+        password = totp.now()
+        self.src = "weakiwi:%s" % str(password)
+
+    def authenticate(self, headers, target_host, target_port):
+        auth_header = headers.get('Authorization')
+        if auth_header:
+            if not auth_header.startswith('Basic '):
+                self.auth_error()
+
+            try:
+                user_pass_raw = base64.b64decode(auth_header[6:])
+            except TypeError:
+                self.auth_error()
+
+            try:
+                # http://stackoverflow.com/questions/7242316/what-encoding-should-i-use-for-http-basic-authentication
+                user_pass_as_text = user_pass_raw.decode('ISO-8859-1')
+            except UnicodeDecodeError:
+                self.auth_error()
+
+            user_pass = user_pass_as_text.split(':', 1)
+            if len(user_pass) != 2:
+                self.auth_error()
+
+            if not self.validate_creds(*user_pass):
+                self.demand_auth()
+
+        else:
+            self.demand_auth()
+
+    def validate_creds(self, username, password):
+        if '%s:%s' % (username, password) == self.src:
+            return True
+        else:
+            return False
+
+    def auth_error(self):
+        raise AuthenticationError(response_code=403)
+
+    def demand_auth(self):
+        raise AuthenticationError(response_code=401,
+                                  response_headers={'WWW-Authenticate': 'Basic realm="Websockify"'})
 
 class BasicHTTPAuth(object):
     """Verifies Basic Auth headers. Specify src as username:password"""
